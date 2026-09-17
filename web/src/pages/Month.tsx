64a271373b90f32c02f0483753662b186f577992
Antoine-NVE <antoinenavette@gmail.com>
2026-09-17 19:40:23 +0200

    chore: restore TODO comment in Month.tsx date range computation

--- web/src/pages/Month.tsx
+++ web/src/pages/Month.tsx
@@ -28,6 +28,7 @@ const Month = () => {
         const fetchData = async () => {
             setState({ status: 'loading' });
 
+            // TODO: clean
             const from = `${year}-${String(month).padStart(2, '0')}-01`;
             const lastDay = new Date(year, month, 0).getDate();
             const to = `${year}-${String(month).padStart(2, '0')}-${String(lastDay).padStart(2, '0')}`;
